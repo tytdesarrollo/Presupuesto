ed238000bbe7b3db095f40ba8afd25842e7915a3
cristianguerrero <c.a.g.9650@hotmail.com>
2017-09-05 10:31:14 -0500

    Sweet alert, calendario, responsive
    cambios en estilo y scripts.
    Adicion de formatos, calendario y responsive.

--- views/site/presupuesto.php
+++ views/site/presupuesto.php
@@ -46,6 +46,16 @@ $this->title = 'Ejecución Presupuestal';
 						<div class="row">
 							<div class="col-sm-12">
 								<h3 class="text-center">Selecciona el periodo a consultar</h3>
+									<div class="row">
+										<div class="col-sm-1 col-xs-2"></div>
+										<div class="col-sm-10 col-xs-6">
+											<label for="from">Desde</label>
+											<input type="text" data-type="date" id="from" name="from">
+											<label for="to">Hasta</label>
+											<input type="text" data-type="date" id="to" name="to">
+										</div>
+										<div class="col-sm-1 col-xs-4"></div>
+									</div>
 							</div>
 						</div>
 						<div class="row">
@@ -63,12 +73,24 @@ $this->title = 'Ejecución Presupuestal';
 											Opción #2 para generar el reporte
 										</label>
 									</div>
+									<div class="radio radio-primary">
+										<label>
+											<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2" disabled>
+											Opción #3 para generar el reporte
+										</label>
+									</div>
+									<div class="radio radio-primary">
+										<label>
+											<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2" disabled>
+											Opción #4 para generar el reporte
+										</label>
+									</div>
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-sm-12">
-								<div class="form-group text-center"><button class="btn btn-primary btn-raised">Generar</button></div>
+								<div class="form-group text-center"><button onclick="Alerta()" class="btn btn-primary btn-raised">Generar</button></div>
 							</div>
 						</div>
 					</div>
@@ -77,3 +99,39 @@ $this->title = 'Ejecución Presupuestal';
 		</div>
 	</div>
 </div>
+
+<script>
+function Alerta(){
+
+var inputOptions = new Promise(function (resolve) {
+  setTimeout(function () {
+    resolve({
+      'EXCEL': 'EXCEL',
+      'PDF': 'PDF'
+    })
+  }, 1000)
+})
+
+swal({
+  title: 'En qué formato?',
+  input: 'radio',
+  inputOptions: inputOptions,
+  inputValidator: function (result) {
+    return new Promise(function (resolve, reject) {
+      if (result) {
+        resolve()
+      } else {
+        reject('Elija un formato!')
+      }
+    })
+  }
+}).then(function (result) {
+  swal({
+    type: 'success',
+    html: 'Seleccionó: ' + result
+  })
+})
+
+
+};
+</script>
